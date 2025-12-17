007a4300fff893e732323a6ca197f7cb995dcd4c
jeehalways <jeeh.always@hotmail.com>
2025-12-17 12:20:14 +0100

    Added firebase SDK into register.html
    Defined API_BASE_URL

--- frontend/js/firebaseConfig.js
+++ frontend/js/firebaseConfig.js
@@ -12,8 +12,8 @@ if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
-// Backend API base URL
-const API_BASE_URL =
+// Backend API base URL (Global)
+window.API_BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:3000"
     : "https://drawing-app-q0sx.onrender.com";
